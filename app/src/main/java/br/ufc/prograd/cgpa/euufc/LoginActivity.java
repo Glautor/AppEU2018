fc453c99680a800e6ce266cac14cf66bcdfaf479
Glauton <glautoncardoso@gmail.com>
2018-09-19 14:45:42 -0300

    :bug: app quebrando depois de clicar muitas vezes no botao de login ajustado

--- app/src/main/java/br/ufc/prograd/cgpa/euufc/LoginActivity.java
+++ app/src/main/java/br/ufc/prograd/cgpa/euufc/LoginActivity.java
@@ -82,6 +82,7 @@ public class LoginActivity extends AppCompatActivity {
 //    private View mLoginFormView;
     TextView loginCpf;
     TextView loginMatricula;
+    boolean click = true;
 
     String resultado = "";
     @Override
@@ -113,22 +114,14 @@ public class LoginActivity extends AppCompatActivity {
 
         cpf.addTextChangedListener(maskCPF);
 
-        // Set up the login form.
-//        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-//        populateAutoComplete();
-
-
-//        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-//
-//
-//        mLoginFormView = findViewById(R.id.login_form);
-//        mProgressView = findViewById(R.id.login_progress);
     }
 
 
     public void login(View view){
 
-        if(loginCpf.getText().toString().replaceAll("[^0-9]", "").length() == 11) {
+
+        if(loginCpf.getText().toString().replaceAll("[^0-9]", "").length() == 11 && click == true) {
+            click = false;
             // Instantiate the RequestQueue.
             RequestQueue queue = Volley.newRequestQueue(this);
             String url = "http://sysprppg.ufc.br/eu/2018/Resumos/api/alunos/" + loginCpf.getText().toString().replaceAll("[^0-9]", "");
@@ -142,12 +135,13 @@ public class LoginActivity extends AppCompatActivity {
                             // Display the first 500 characters of the response string.
                             resultado = response.toString();
                             mAuthTask = new UserLoginTask(loginCpf.getText().toString().replaceAll("[^0-9]", ""), "0");
-                            mAuthTask.execute((Void) null);
+                            mAuthTask.execute();
                         }
                     }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
                     Toast.makeText(getApplicationContext(), "Não conseguimos nos conectar ao servidor", Toast.LENGTH_LONG).show();
+                    click = true;
                     //textView1.setText("That didn't work!");
                 }
             });
@@ -156,50 +150,11 @@ public class LoginActivity extends AppCompatActivity {
             queue.add(stringRequest);
         }else{
             loginCpf.setError(getString(R.string.cpf_incomplete));
+            click = true;
         }
+
     }
-//    private void populateAutoComplete() {
-//        if (!mayRequestContacts()) {
-//            return;
-//        }
-//
-//        getLoaderManager().initLoader(0, null, this);
-//    }
 
-//    private boolean mayRequestContacts() {
-//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-//            return true;
-//        }
-//        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-//            return true;
-//        }
-//        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-//            Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-//                    .setAction(android.R.string.ok, new View.OnClickListener() {
-//                        @Override
-//                        @TargetApi(Build.VERSION_CODES.M)
-//                        public void onClick(View v) {
-//                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-//                        }
-//                    });
-//        } else {
-//            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-//        }
-//        return false;
-//    }
-
-//    /**
-//     * Callback received when a permissions request has been completed.
-//     */
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-//                                           @NonNull int[] grantResults) {
-//        if (requestCode == REQUEST_READ_CONTACTS) {
-//            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                populateAutoComplete();
-//            }
-//        }
-//    }
 
 
     private boolean isEmailValid(String email) {
@@ -216,100 +171,6 @@ public class LoginActivity extends AppCompatActivity {
         return cpf.length() >= 4;
     }
 
-//    /**
-//     * Shows the progress UI and hides the login form.
-//     */
-//    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-//    private void showProgress(final boolean show) {
-//        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-//        // for very easy animations. If available, use these APIs to fade-in
-//        // the progress spinner.
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-//            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-//
-//            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-//            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-//                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-//                @Override
-//                public void onAnimationEnd(Animator animation) {
-//                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-//                }
-//            });
-//
-//            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-//            mProgressView.animate().setDuration(shortAnimTime).alpha(
-//                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-//                @Override
-//                public void onAnimationEnd(Animator animation) {
-//                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-//                }
-//            });
-//        } else {
-//            // The ViewPropertyAnimator APIs are not available, so simply show
-//            // and hide the relevant UI components.
-//            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-//            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-//        }
-//    }
-
-//    @Override
-//    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-//        return new CursorLoader(this,
-//                // Retrieve data rows for the device user's 'profile' contact.
-//                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-//                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-//
-//                // Select only email addresses.
-//                ContactsContract.Contacts.Data.MIMETYPE +
-//                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-//                .CONTENT_ITEM_TYPE},
-//
-//                // Show primary email addresses first. Note that there won't be
-//                // a primary email address if the user hasn't specified one.
-//                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-//    }
-//
-//    @Override
-//    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-//        List<String> emails = new ArrayList<>();
-//        cursor.moveToFirst();
-//        while (!cursor.isAfterLast()) {
-//            emails.add(cursor.getString(ProfileQuery.ADDRESS));
-//            cursor.moveToNext();
-//        }
-//
-//        addEmailsToAutoComplete(emails);
-//    }
-//
-//    @Override
-//    public void onLoaderReset(Loader<Cursor> cursorLoader) {
-//
-//    }
-//
-//    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-//        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-//        ArrayAdapter<String> adapter =
-//                new ArrayAdapter<>(LoginActivity.this,
-//                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-//
-//        mEmailView.setAdapter(adapter);
-//    }
-//
-//
-//    private interface ProfileQuery {
-//        String[] PROJECTION = {
-//                ContactsContract.CommonDataKinds.Email.ADDRESS,
-//                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-//        };
-//
-//        int ADDRESS = 0;
-//        int IS_PRIMARY = 1;
-//    }
-//
-//    /**
-//     * Represents an asynchronous login/registration task used to authenticate
-//     * the user.
-//     */
 
     public void saveInfoLogin(String cpf, String nome, int id){
         SharedPreferences infoLogin = getSharedPreferences(LOGIN_ARQUIVO,0);
@@ -378,12 +239,13 @@ public class LoginActivity extends AppCompatActivity {
 
             if (mCpf.equals(user.getCpf()) &&  mMatricula.equals(String.valueOf(user.getMatricula()))) {
 
+                click = false;
                 saveInfoLogin(user.getCpf(),user.getNome(), user.getId());
                 Intent intent = new Intent(getApplicationContext(), Home.class);
                 startActivity(intent);
                 finalizaActivity();
             } else {
-
+                click = true;
                 //loginMatricula.setError(getString(R.string.error_user_not_found));
                 loginCpf.setError(getString(R.string.error_user_not_found));
             }
@@ -399,6 +261,7 @@ public class LoginActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute(){
             load = ProgressDialog.show(LoginActivity.this, "Por favor Aguarde ...", "Recuperando Informações do Servidor...");
+
         }
     }
 
